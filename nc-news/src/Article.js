be447e770e8dc870f24cc11f0113d04a0f8b3a66
James Steele <jamessteele87@outlook.com>
2018-01-18 11:58:47 +0000

    add user name and ability to click on name

--- nc-news/src/Article.js
+++ nc-news/src/Article.js
@@ -10,43 +10,43 @@ class Article extends React.Component {
   componentDidMount() {
     const articleId = this.props.match.params.article_id;
     fetch(`https://northcoders-news-api.herokuapp.com/api/articles/${articleId}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((article) => {
-      this.setState({
-        articleInfo: article
+      .then((res) => {
+        return res.json();
       })
-    })
+      .then((article) => {
+        this.setState({
+          articleInfo: article
+        })
+      })
     // .then((articleInfo) => {
-      fetch(`https://northcoders-news-api.herokuapp.com/api/articles/${articleId}/comments`)
-    // })
-    .then((commentInfo) => {
-      return commentInfo.json();
-    })
-    .then((comment) => {
-      const commentList =comment.comments
-      this.setState({
-        comments: commentList
+    fetch(`https://northcoders-news-api.herokuapp.com/api/articles/${articleId}/comments`)
+      // })
+      .then((commentInfo) => {
+        return commentInfo.json();
       })
-    })
+      .then((comment) => {
+        const commentList = comment.comments
+        this.setState({
+          comments: commentList
+        })
+      })
   }
-  
+
   render() {
-    console.log(this.state.comments)
     const articleInfo = this.state
     return (
       <div>
         <h2>Article Page</h2>
-       <h3>{articleInfo.articleInfo.title}</h3>
-       <p>{articleInfo.articleInfo.body}</p>
-       <Link to={`/users/${articleInfo.articleInfo.created_by}`}>by {articleInfo.articleInfo.created_by} </Link>
-       <p>likes: {articleInfo.articleInfo.votes}</p>
-       <h3>Comments</h3>
-       {this.state.comments.map((comment, index) => (
+        <h3>{articleInfo.articleInfo.title}</h3>
+        <p>{articleInfo.articleInfo.body}</p>
+        <Link to={`/users/${articleInfo.articleInfo.created_by}`}>by {articleInfo.articleInfo.created_by} </Link>
+        <p>likes: {articleInfo.articleInfo.votes}</p>
+        <h3>Comments</h3>
+        {this.state.comments.map((comment, index) => (
           <div key={index}>
             <p>{comment.body}</p>
-              <p>likes {comment.votes}</p>
+            <Link to={`/users/${comment.created_by}`}> Added by {comment.created_by} </Link>
+            <p>likes {comment.votes}</p>
             <hr />
           </div>
         )
