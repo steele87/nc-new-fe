8393bd3768f98632d3e47cbb5529795b99c3a441
James Steele <jamessteele87@outlook.com>
2018-02-08 19:28:59 +0000

    add style to user pages

--- nc-news/src/components/UserComponent.js
+++ nc-news/src/components/UserComponent.js
@@ -24,7 +24,7 @@ class UserComponent extends React.Component {
       <div>
         <h2><i className="fas fa-user"></i> User Page</h2>
         {this.state.user.map((user, index) => (
-          <div key={index}>
+          <div className="frownFace" key={index}>
             <img src={`${user.avatar_url}`} alt="user" style={{ width: 150 }} className="profileImg" />
             <p>Name: {user.name}</p>
             <p>username: {user.username} </p>
